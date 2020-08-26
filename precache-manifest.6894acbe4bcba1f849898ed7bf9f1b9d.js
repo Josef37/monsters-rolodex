0eebcd3b55579cbed3c5b0c39c40f781f0bcddd3
Josef Wittmann <josefwittmann42@gmail.com>
2020-08-26 07:32:16 +0200

    Updates

---
+++ precache-manifest.6894acbe4bcba1f849898ed7bf9f1b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f7b535de0456c199e9a8caeb3c6c37",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9f5b8aea31275c8148b8",
+    "url": "/monsters-rolodex/static/css/main.144bdf1a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5b8aea31275c8148b8",
+    "url": "/monsters-rolodex/static/js/main.70a55983.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
